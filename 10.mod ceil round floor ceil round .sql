e1bce39744b1f9419a43e1da61b4aae29c947e97
sunxiao <qinglangsalaheiyou@163.com>
2019-09-13 13:05:30 +0800

    10.mod ceil round floor ceil round

---
+++ 10.mod ceil round floor ceil round .sql
@@ -0,0 +1,17 @@
+use myemployees;
+
+select database();
+
+select round(1.11);
+select round(-1.4);
+select round(1.11,1);
+
+
+select ceil(1.666);
+select ceil(-1.34);
+
+select floor(1.2222);
+
+# select(a,b)  a-a/b*b
+select mod(10,3);
+
