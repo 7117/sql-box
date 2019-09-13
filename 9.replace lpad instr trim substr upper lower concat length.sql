c65c4245d31df521d61fdb8171621f01cfd5b3b0
sunxiao <qinglangsalaheiyou@163.com>
2019-09-13 12:43:45 +0800

    9.replace lpad instr trim substr upper lower concat length

--- 9.replace lpad instr trim substr upper lower concat length.sql
+++ 9.replace lpad instr trim substr upper lower concat length.sql
@@ -10,3 +10,18 @@ select concat('aaa','_','bbb')
 select upper("aaaa");
 select lower("AAAA");
 select concat(upper(last_name),lower(first_name)) 项目 from employees;
+
+# substr索引从1开始
+select substr('杀出重围错车务段',6);
+select substr('杀出重围错车务段',6,1);
+# instr  5
+select instr("avbgsa","s")
+# trim
+select trim("   d   ")
+select length(trim("    ddd    "))
+select trim("d" from "ddddddssssdddddd")
+# lpad
+select lpad("aaa",10,"d");
+select rpad("aaa",10,"d");
+# replace
+select replace('aaaaaaaaaa','a','b');
