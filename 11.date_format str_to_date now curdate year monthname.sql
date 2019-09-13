40ac7f62488929525c97424bb4ec0746b9dd4c31
sunxiao <qinglangsalaheiyou@163.com>
2019-09-13 15:56:14 +0800

    时间函数

--- 11.date_format str_to_date now curdate year monthname.sql
+++ 11.date_format str_to_date now curdate year monthname.sql
@@ -1,17 +1,18 @@
 use myemployees;
+show tables ;
 
-select database();
+select now();
+select curdate();
+select curtime();
+select year(now());
+select year('1000-1-1');
+select monthname('1000-1-1');
+# 将字符转化为时间
+select str_to_date('2312-1-11 11:22:33','%Y-%c-%d %H:%i:%s');
+select str_to_date('4-3 1990','%m-%d %Y');
+# 将时间转化为字符
+select date_format('2312/01/21','%y年%d月%d日');
+select date_format(now(),'%Y年%d月%d日');
+select date_format(now(),'%y年%d月%d日');
 
-select round(1.11);
-select round(-1.4);
-select round(1.11,1);
-
-
-select ceil(1.666);
-select ceil(-1.34);
-
-select floor(1.2222);
-
-# select(a,b)  a-a/b*b
-select mod(10,3);
-
+select commission_pct,date_format(hiredate,'%Y年%m月%d日') from employees where commission_pct is not null;
