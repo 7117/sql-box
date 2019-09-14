c6aa9826b4dff6f17bec3e15df58ae26837ad872
sunxiao <qinglangsalaheiyou@163.com>
2019-09-14 12:39:19 +0800

    非等值连接between in

--- 21.非等值连接 自连接.sql
+++ 21.非等值连接 自连接.sql
@@ -1,14 +1,17 @@
 select  database();
 --# 非等值连接
 --#查询员工的工资级别
---#查询工资级别的个数>20的个数，并且按工资级别降序
 select employees.last_name,grade_level from employees inner join job_grades
 on employees.salary between job_grades.lowest_sal and highest_sal;
 
-select count(*),grade_level from job_grades inner join employees
-on employees.salary between job_grades.lowest_sal and highest_sal
-group by grade_level
-having count(*)>20;
+--  #查询工资级别的个数>20的个数，并且按工资级别降序
+ SELECT COUNT(*),grade_level
+FROM employees e
+ JOIN job_grades g
+ ON e.`salary` BETWEEN g.`lowest_sal` AND g.`highest_sal`
+ GROUP BY grade_level
+ HAVING COUNT(*)>20
+ ORDER BY grade_level DESC;
 --# 自连接
 --#查询员工的名字、上级的名字
 select e.last_name,m.last_name from employees e inner join employees m
