6bd392c29ee8be11b8e27d63a76a014f1c8ffe9d
sunxiao <qinglangsalaheiyou@163.com>
2019-09-14 11:57:25 +0800

    外连接练习题目

--- 21.非等值连接 自连接.sql
+++ 21.非等值连接 自连接.sql
@@ -1,7 +1,7 @@
 select  database();
-# 非等值连接
-#查询员工的工资级别
-#查询工资级别的个数>20的个数，并且按工资级别降序
+--# 非等值连接
+--#查询员工的工资级别
+--#查询工资级别的个数>20的个数，并且按工资级别降序
 select employees.last_name,grade_level from employees inner join job_grades
 on employees.salary between job_grades.lowest_sal and highest_sal;
 
@@ -9,8 +9,8 @@ select count(*),grade_level from job_grades inner join employees
 on employees.salary between job_grades.lowest_sal and highest_sal
 group by grade_level
 having count(*)>20;
-# 自连接
-#查询员工的名字、上级的名字
+--# 自连接
+--#查询员工的名字、上级的名字
 select e.last_name,m.last_name from employees e inner join employees m
 on e.manager_id=m.employee_id
 WHERE e.`last_name` LIKE '%ke%';
--- 22.外连接 cross join.sql
+++ 22.外连接 cross join.sql
@@ -4,7 +4,7 @@ select beauty.name,boys.boyName from beauty left join boys
 on beauty.boyfriend_id=boys.id
 where boyName is null;
 
-#案例1：查询哪个部门没有员工
+--#案例1：查询哪个部门没有员工
 use myemployees;
 select database();
 select departments.department_id,employees.employee_id from
@@ -13,5 +13,26 @@ departments.department_id=employees.department_id
 where employees.employee_id is null;
 
 select boys.*,beauty.* from beauty cross join boys;
+
+--#一、查询编号>3的女神的男朋友信息，如果有则列出详细，如果没有，用null填充
+--#二、查询哪个城市没有部门
+--#三、查询部门名为SAL或IT的员工信息
+
+select boyname,name from beauty left join boys
+on beauty.boyfriend_id=boys.id
+where beauty.id>3;
+
+-- 以下是练习题目
+select database();
+
+-- #一、查询编号>3的女神的男朋友信息，如果有则列出详细，如果没有，用null填充
+select boyName,name from beauty left join boys on boyfriend_id=boys.id
+where beauty.id>3;
+-- #二、查询哪个城市没有部门
+select city,department_name from locations left join departments d on locations.location_id = d.location_id
+where department_name is null;
+-- #三、查询部门名为SAL或IT的员工信息
+select department_name,employees.last_name ,first_name from employees left join departments d on employees.department_id = d.department_id
+where department_name in ('IT','Sal');
 
 
