6b5cf157f6e30fbddee618c70d888350ee0e0895
sunxiao <qinglangsalaheiyou@163.com>
2019-09-15 23:35:08 +0800

    40.表级约束列级约束 3种法-表级列级、普通、写法、constraint references foreign记单词！

--- 40.表级约束列级约束 算是3种写法-表级列级、普通、写法.sql
+++ 40.表级约束列级约束 算是3种写法-表级列级、普通、写法.sql
@@ -50,3 +50,15 @@ insert into major values (1,'aa');
 insert into major values (2,'bb');
 
 select * from bb left join major m on bb.major_id = m.id where bb.major_id=1;
+
+# 普通写法
+create table kk(
+  id int(11) primary key not null auto_increment,
+  name varchar(11) default "我的",
+  sex char(2) check(sex in('男','女')),
+  address varchar(11) unique,
+  constraint fk foreign key(sex) references major(id),
+  constraint fk2 foreign key(name) references major(name),
+  foreign key(id) references major(id)
+);
+desc kk;
