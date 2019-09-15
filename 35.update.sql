d8c4ed47e30a5ce617d4e63fc1dca1cb8f49a685
sunxiao <qinglangsalaheiyou@163.com>
2019-09-15 18:06:38 +0800

    update:单表  连表  如果没有where就是修改整张表的哦

--- 35.update.sql
+++ 35.update.sql
@@ -1,10 +1,10 @@
 select * from beauty;
-#案例1：修改beauty表中姓唐的女神的电话为13899888899
+#单表：案例1：修改beauty表中姓唐的女神的电话为13899888899
 update beauty set phone='11111' where name like '%aa%';
 update beauty set phone='a',sex='女' where name like '%aa%';
-#案例 1：修改张无忌的女朋友的手机号为114
+#连表：案例 1：修改张无忌的女朋友的手机号为114
 update beauty inner join boys on beauty.boyfriend_id=boys.id
 set beauty.name='ff' where boys.id=5;
-#案例1：修改没有男朋友的女神的男朋友编号都为2号
+#连表：案例1：修改没有男朋友的女神的男朋友编号都为2号
 UPDATE boys bo RIGHT JOIN beauty b ON bo.`id`=b.`boyfriend_id` SET b.`boyfriend_id`=2
 WHERE bo.`id` IS NULL;
