c97081ae5f746da00166985f8ae6b97f14d25058
sunxiao <qinglangsalaheiyou@163.com>
2019-09-17 15:53:24 +0800

    11.date_format str_to_date后面第一个参数是时间或者字符串 第二个参数是时间格式 now curdate year monthname.sql

--- 11.date_format str_to_date后面第一个参数是时间或者字符串 第二个参数是时间格式 now curdate year monthname.sql
+++ 11.date_format str_to_date后面第一个参数是时间或者字符串 第二个参数是时间格式 now curdate year monthname.sql
@@ -7,10 +7,10 @@ select curtime();
 select year(now());
 select year('1000-1-1');
 select monthname('1000-1-1');
-# 将字符转化为时间
+-- # 将字符转化为时间
 select str_to_date('2312-1-11 11:22:33','%Y-%c-%d %H:%i:%s');
 select str_to_date('4-3 1990','%m-%d %Y');
-# 将时间转化为字符
+-- # 将时间转化为字符
 select date_format('2312/01/21','%y年%d月%d日');
 select date_format(now(),'%Y年%d月%d日');
 select date_format(now(),'%y年%d月%d日');
