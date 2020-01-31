dd9a10711dd1fa39eaf28e3f494983c2c5a2f090
7117 <qinglangsalaheiyou@163.com>
2020-01-31 20:49:05 +0800

    76.行锁

--- 76.行锁.sql
+++ 76.行锁.sql
@@ -13,3 +13,12 @@ commit;
 # [2020-01-31 11:40:29] 500 rows retrieved starting from 1 in 151 ms (execution: 22 ms, fetching: 129 ms)
 # sql> update emp set ename='aaa' where id=2
 # [2020-01-31 11:41:38] 1 row affected in 11 s 974 ms
+
+
+#查看innodb引擎的分析情况
+show databases;
+use test1;
+
+show status like 'innodb_row_lock%';
+
+
