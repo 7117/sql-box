45db626f4feb4af3ad3efccb2054445019a040e9
7117 <qinglangsalaheiyou@163.com>
2020-06-08 09:47:50 +0800

    code

--- 2.注释 isnull.sql
+++
@@ -1,9 +0,0 @@
-﻿--#注释的三种方式
---# 方法一：#
---#方法二：-- 杠杠空格
---#方法三：/**/
---show databases ;
-
-
-
-select isnull(commission_pct),commission_pct from employees;
--- 8.排序.sql
+++
@@ -1,6 +0,0 @@
-use myemployees;
-
-select employee_id,salary from employees order by salary desc;
-select employee_id,salary from employees order by salary asc;
-
-
